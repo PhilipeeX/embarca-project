c5170df45a0fb45f7dc8999165c3da12c3102d48
Philipe Rodrigues <dev.philipe@outlook.com>
2024-02-01 10:42:09 -0300

    chore(i18n): Translates state model and views

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module EmbarcaProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Defines available locales i18n pt-BR
+    config.i18n.default_locale = 'pt-BR'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
