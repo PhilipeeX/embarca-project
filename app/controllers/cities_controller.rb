bc30f89d9d6dda6f9bacd6e7cd6545247d8dd0b2
Philipe Rodrigues <dev.philipe@outlook.com>
2024-02-02 14:54:12 -0300

    feat(Filters): Adds filter by state on index

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,5 +4,11 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
+    @states = State.all
+    @selected_state = params[:state_id]
+
+    if @selected_state.present?
+      @cities = @cities.where(state_id: @selected_state)
+    end
   end
 end
